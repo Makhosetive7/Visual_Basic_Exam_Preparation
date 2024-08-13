54a1cf38f5334aa57aa45b924e3fbb305674929d
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-08-13 21:22:21 +0200

    practised division with exception handling and modulo operation with user feedback

---
+++ Simple Arithmetic/Division with exception handling/Division with exception Handling.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+        'Design a VB.NET program that divides one integer by another. The program should handle the 
+        'scenario where the user attempts to divide by zero by displaying an error message instead of crashing.
+
+        Dim first_Number As Integer
+        Dim second_Number As Integer
+        Dim Answer As Integer
+
+        Console.WriteLine("Please enter the first number")
+        first_Number = Console.ReadLine()
+
+        Console.WriteLine("Please enter the second number")
+        second_Number = Console.ReadLine()
+
+        Try
+
+            Answer = first_Number \ second_Number
+            Console.WriteLine("the answer is {0}, {1} / {2} = {0}", Answer, first_Number, second_Number)
+
+        Catch ex As DivideByZeroException
+            Console.WriteLine("trying to devide by 0 will definately produce an error")
+        End Try
+
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
---
+++ Simple Arithmetic/Modulo Operation with User Feedback/Modulo Operation with User Feedback.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+    'Write a VB.NET application that calculates the remainder when an integer is divided by 
+    'another integer. If the remainder Is zero, inform the user that the first number Is perfectly divisible by 
+    'the second.
+
+    Sub Main()
+        Dim first_Number As Integer
+        Dim second_number As Integer
+        Dim answer As Integer
+
+        Console.WriteLine("Please enter first number")
+        first_Number = Console.ReadLine()
+
+        Console.WriteLine("Please enter second number")
+        second_number = Console.ReadLine()
+
+        answer = first_Number Mod second_number
+        Console.WriteLine("the remainder is {0}, {1} Mod {2} = {0} ", answer, first_Number, second_number)
+
+        If answer = 0 Then
+            Console.WriteLine("the first number Is perfectly divisible by the second")
+        Else
+            Console.WriteLine("oooooops")
+        End If
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
