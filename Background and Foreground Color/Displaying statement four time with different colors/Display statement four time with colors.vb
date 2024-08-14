6183287ed11b36f41d9a802a3ddb0b2db8b615c6
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-08-14 10:35:32 +0200

    practised implementing background and foreground colors in loops and solved quadratic equation

---
+++ Background and Foreground Color/Displaying statement four time with different colors/Display statement four time with colors.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+    'write a VB.NET console application that displays the statement "I LOVE YOU FOUR TIME WITH DIFFERENT FORE AND  BACK COLORS"
+    Sub Main()
+
+        Dim line_One As String = "i Love you"
+        Dim line_Two As String = "i Love you"
+        Dim line_Three As String = "i Love you"
+        Dim line_Four As String = "i Love you"
+
+        For i As Integer = 1 To 4
+            Select Case i
+                Case 1
+                    Console.ForegroundColor = ConsoleColor.Red
+                    Console.BackgroundColor = ConsoleColor.Green
+                    Console.WriteLine(line_One)
+                Case 2
+                    Console.ForegroundColor = ConsoleColor.Yellow
+                    Console.BackgroundColor = ConsoleColor.Magenta
+                    Console.WriteLine(line_Two)
+                Case 3
+                    Console.ForegroundColor = ConsoleColor.Blue
+                    Console.BackgroundColor = ConsoleColor.DarkCyan
+                    Console.WriteLine(line_Three)
+                Case 4
+                    Console.ForegroundColor = ConsoleColor.White
+                    Console.BackgroundColor = ConsoleColor.Black
+                    Console.WriteLine(line_Four)
+            End Select
+
+            Console.ReadKey()
+        Next
+
+    End Sub
+
+End Module
---
+++ Simple Arithmetic/Quadratic Equation/Quadratic Equetion.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+    'Write a program that accepts the coefficients 𝑎, 𝑏, And 𝑐 of a quadratic equation 𝑎𝑥2+𝑏𝑥+𝑐=0 And calculates the roots using the quadratic formula.
+
+    Sub Main()
+        Dim coeffient_A As Double
+        Dim coeffient_B As Double
+        Dim coeffient_C As Double
+
+        Console.WriteLine("please enter 1st coefficient")
+        coeffient_A = Console.ReadLine()
+
+        Console.WriteLine("please enter 2nd coefficient")
+        coeffient_B = Console.ReadLine()
+
+        Console.WriteLine("please enter 3rd coefficient")
+        coeffient_C = Console.ReadLine()
+
+        Dim discriminate = (coeffient_B * coeffient_B) = (4 * coeffient_A * coeffient_C)
+
+        If discriminate > 0 Then
+            Dim root_A As Double = (-coeffient_B + Math.Sqrt(discriminate) / (2 * coeffient_A))
+            Dim root_B As Double = (-coeffient_B - Math.Sqrt(discriminate) / (2 * coeffient_A))
+            Console.WriteLine("the roots are distinct and real: {0} and {1}", root_A, root_B)
+
+        ElseIf discriminate = 0 Then
+            Dim root_A As Double = -coeffient_B / (2 * coeffient_A)
+            Console.WriteLine("Roots are real and equal: {0}", root_A)
+        Else
+            Console.WriteLine("The roots are complex .")
+
+
+        End If
+        Console.ReadKey()
+    End Sub
+
+End Module
