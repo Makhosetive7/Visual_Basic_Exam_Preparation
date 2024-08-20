99c11db74b5f8b2858b362079a14343ccefa863b
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-08-20 02:32:28 +0200

    revisited visual basic structures

---
+++ Simple Arithmetic/Exponent program/exponent program.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+    'Create a VB.NET program that asks the user for a base number and an exponent. The program 
+    'should calculate the result Of raising the base To the power Of the exponent And display the result.
+
+    Sub Main()
+        Dim baseNumber As Double
+        Dim exponent As Double
+        Dim result As Double
+
+        Console.Write("Enter the base number: ")
+        baseNumber = Console.ReadLine()
+
+        Console.Write("Enter the exponent: ")
+        exponent = Console.ReadLine()
+
+        result = Math.Pow(baseNumber, exponent)
+
+        Console.WriteLine("{0} raised to the power of {1} is {3}", baseNumber, exponent, result)
+
+
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ Structure/Book details formating/Book details.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Runtime.Remoting.Channels
+
+Module Module1
+    'Define a structure named Book that contains Title (String), Author (String), 
+    'And ISBN (String). Create a method within the structure named FormatBookDetails that 
+    'returns the book details In the format "Title by Author, ISBN: ISBN".
+
+
+    Public Structure Book
+        Dim title As String
+        Dim author As String
+        Dim ISBN As String
+
+        Public Function FormatedBookDetails() As String
+            Return String.Format("{0} by {1}, ISBN: {2}", title, author, ISBN)
+        End Function
+    End Structure
+
+
+    Sub Main()
+        Dim newBook As Book = New Book()
+
+        Console.WriteLine("Please enter Book title")
+        newBook.title = Console.ReadLine()
+
+        Console.WriteLine("Please enter author of Book")
+        newBook.author = Console.ReadLine()
+
+        Console.WriteLine("Please enter Book ISBN")
+        newBook.ISBN = Console.ReadLine()
+
+        Console.WriteLine(newBook.FormatedBookDetails)
+
+        Console.ReadKey()
+    End Sub
+
+End Module
