a447403f08e35d07865aae4aafd26f36570b045f
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-09-06 10:23:39 +0200

    revised: array manipulation in visual basic

---
+++ Arrays/array_Reversal/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+    'Write a Visual Basic program that prompts the user To enter 5 integers, stores them
+    'in a single-dimensional array, And then displays the array in reverse order
+
+    Sub Main()
+        Dim array_integers(4) As Integer
+        'user input
+        For i As Integer = 0 To array_integers.Length - 1
+            Console.WriteLine("Please enter array integers:")
+            array_integers(i) = Console.ReadLine
+        Next
+
+        'array reversal
+        Console.WriteLine("the reversed order if entered array integers is:")
+        Array.Reverse(array_integers)
+
+        For Each num As Integer In array_integers
+            Console.WriteLine(num)
+        Next
+
+
+
+        Console.ReadKey()
+
+    End Sub
+
+
+End Module
---
+++ Arrays/array_manipulation_windowsForm/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+
+    Dim array_list() As Integer = {100, 40, 30, 70, 50}
+
+    'button to add Array list to the listbox
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Add("adding array list :")
+        For i As Integer = 0 To array_list.Length - 1
+            ListBox1.Items.Add(array_list(i))
+        Next
+    End Sub
+
+    'button to sort Array list in the listbox
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Array.Sort(array_list)
+        ListBox1.Items.Add("Sorted array list :")
+        For Each i As Integer In array_list
+
+            ListBox1.Items.Add(i)
+        Next
+    End Sub
+
+    'button to reverse Array list
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Array.Reverse(array_list)
+        ListBox1.Items.Add("Reversed array list :")
+        For Each i As Integer In array_list
+            ListBox1.Items.Add(i)
+        Next
+
+    End Sub
+End Class
