1b2a08d935748dfdb3f4c42fd706e46d06a93364
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-09-09 01:32:44 +0200

    practised dates in visual basic windows form

---
+++ date_and_time/car_rental_selector_windowsForm/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    'Create a Windows Forms application that allows users to select a rental start date and
+    'Return Date Using DateTimePicker controls. Calculate the number Of days between the
+    'two dates And display the total rental days In a label
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim start_date As DateTime = DateTimePicker1.Value
+        Dim end_date As DateTime = DateTimePicker2.Value
+        Dim return_date As Long
+
+        return_date = DateDiff(DateInterval.Day, start_date, end_date)
+
+        Label3.Text = return_date
+    End Sub
+End Class
