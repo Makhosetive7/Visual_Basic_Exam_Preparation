d19e153845ab5523efee33b5ddb25439ba8a55e2
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-09-08 10:29:46 +0200

    practised simple arithmetic in visual basic

---
+++ Random questions/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Dim rnd As New Random
+
+    Dim first_number As Integer = rnd.Next(0, 10)
+    Dim second_number As Integer = rnd.Next(0, 10)
+    Dim third_number As Integer = rnd.Next(0, 10)
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = first_number.ToString()
+        Label2.Text = second_number.ToString()
+        Label3.Text = third_number.ToString()
+
+        If first_number = 7 And second_number = 7 And third_number = 7 Then
+            PictureBox1.Visible = True
+
+        Else
+            PictureBox1.Visible = False
+        End If
+    End Sub
+End Class
---
+++ Simple Arithmetic/sale_revenue/Module1.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    'write a console application to calculate the revenue from sales based on the unit price and
+    'the quantity of the product input by the user
+    'the discount rate is 10% for the quantity purchased between 100 and 200 units
+    '15% for the quantity puchased greater than 120 units 
+    'if the quantity purchased is less than 100 units, the discount rate is 0%
+
+    Sub Main()
+        Dim unit_price As Integer = 500
+        Dim quantity As Integer
+        Dim total As Integer
+        Dim discounted_total As Integer
+        Dim discount_rate As Double = 0.0
+
+        Console.WriteLine("please enter the quantity you want purchase")
+        quantity = Console.ReadLine()
+
+        If quantity >= 100 Or quantity <= 120 Then
+            discount_rate = 0.1
+        ElseIf quantity > 120 Then
+            discount_rate = 0.15
+            Else
+                discount_rate = 0
+
+
+        End If
+
+
+        total = quantity * unit_price
+        discounted_total = total - (total * discount_rate)
+
+        Console.WriteLine("the total you have to pay is {0}", discounted_total)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
