b7232b70580285f0e232f5bdc597956a699b6d0a
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-09-10 09:45:50 +0200

    revised array manipulation in visual basic

---
+++ Arrays/average_of_array_elements/Module1.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+    'Write a Visual Basic program that asks the user to input 8 floating-point numbers,
+    'stores them In a Single-dimensional array, And Then calculates And displays the average Of those
+    'numbers
+
+    Sub Main()
+        Dim inputed_numbers(3) As Decimal
+        Dim calculate_average As Decimal
+        Dim sum As Decimal = 0
+
+
+        For i As Integer = 0 To inputed_numbers.Length - 1
+            Console.WriteLine("please input number" & i + 1 & ":")
+            inputed_numbers(i) = Console.ReadLine
+            sum += inputed_numbers(i)
+        Next
+
+
+        Console.WriteLine("the inputed array is:")
+        For Each number In inputed_numbers
+            Console.WriteLine(number)
+        Next
+
+        calculate_average = sum / 4
+        Console.WriteLine("the average of array elements is: {0}", calculate_average)
+
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ Arrays/counting_specific_elements/Module1.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Runtime.CompilerServices
+
+Module Module1
+    'Write a Visual Basic program that initializes an array with 10 integers. The program
+    'should prompt the user To enter a specific Integer And Then count how many times that Integer appears
+    'in the array.
+
+    Sub Main()
+        Dim initialized_array(4) As Integer
+        Dim specific_integer As Integer
+        Dim count As Integer = 0
+
+        For i As Integer = 0 To 4
+            Console.WriteLine("Please enter array integers:" & (i + 1) & ":")
+            initialized_array(i) = Console.ReadLine()
+
+        Next
+
+        Console.WriteLine("Please enter specific integer:")
+        specific_integer = Console.ReadLine()
+
+        For i As Integer = 0 To 4
+            If initialized_array(i) = specific_integer Then
+                count += 1
+            End If
+        Next
+
+        Console.WriteLine("Output counts: {0}", count)
+
+
+
+
+        Console.ReadLine()
+    End Sub
+End Module
