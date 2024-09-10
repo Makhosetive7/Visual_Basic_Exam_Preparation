26992ef3f6c8005db6de6941e0a589c3b7522555
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-09-10 03:50:53 +0200

    practised: manipulation of message boxes

---
+++ GUI/GUI_msgBox_buttons/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Drawing.Printing
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Printer not connected", "Printer error...", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MessageBox.Show("File not found", "opening a file", MessageBoxButtons.OKCancel)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MessageBox.Show("Are you show you want delete this file", "Confirm deletion", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        MessageBox.Show("System is shutting down, please continue", "System shutdown", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation)
+    End Sub
+End Class
