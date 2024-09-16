8ca65e5b79e6eca0fe2cee339c893f4f1d02ee42
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-09-16 09:23:29 +0200

    practised multiplication table using for loop and ebabling and disabling button manipulation

---
+++ Random questions/Button_manipulation/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    '"Design an interface with a frame containing two textboxes labelled keyboard and mouse and another frame with two option buttons enabled and disabled respectively. Write VB code such that when a user selects the option disabled, the textboxes will be grayed, and when the user selects the option enabled, the textboxes will be enabled
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Enabled = True
+        TextBox2.Enabled = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Enabled = False
+        TextBox2.Enabled = False
+    End Sub
+End Class
---
+++ Simple Arithmetic/Multiplication_table/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+    'program to display the multiplication table of a number
+    Sub Main()
+        Dim number As Integer
+        Dim multiplication As Integer
+
+        Console.WriteLine("Please enter number:")
+        number = Console.ReadLine()
+
+        Console.WriteLine("the multiplication table of{0}:", number)
+        For i As Integer = 0 To 12
+            multiplication = i * number
+            Console.WriteLine("{0} * {1} = {2}", i, number, multiplication)
+        Next
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
