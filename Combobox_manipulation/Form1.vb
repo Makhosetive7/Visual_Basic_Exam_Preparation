cabcb0561bd93a6d4a0e46488b199d31afae55f3
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-09-17 02:38:06 +0200

    practised combobox and treeview manipulation in visual basic windows form

---
+++ Combobox_manipulation/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    'adding items
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Add(ComboBox1.SelectedItem)
+    End Sub
+
+    'populates the combobox
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ComboBox1.Items.Add("2020")
+        ComboBox1.Items.Add("2021")
+        ComboBox1.Items.Add("2022")
+        ComboBox1.Items.Add("2023")
+        ComboBox1.Items.Add("2024")
+    End Sub
+
+    'sorting items
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Sorted = True
+    End Sub
+
+    'clearing items
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ListBox1.Items.Clear()
+    End Sub
+End Class
---
+++ TreeView/adding_treeView/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "Tree view adding"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim treeView_node As TreeNode = TreeView1.Nodes.Add("Fruits")
+
+        treeView_node.Nodes.Add("apple")
+        treeView_node.Nodes.Add("orange")
+
+        Dim banana_node As TreeNode = treeView_node.Nodes.Add("banana")
+        banana_node.Nodes.Add("yellow banana")
+        banana_node.Nodes.Add("green banana")
+        banana_node.Nodes.Add("red banana")
+
+        Dim mangoe_node As TreeNode = treeView_node.Nodes.Add("mangoe")
+        mangoe_node.Nodes.Add("green mangoe")
+        mangoe_node.Nodes.Add("parafin mangoe")
+    End Sub
+End Class
