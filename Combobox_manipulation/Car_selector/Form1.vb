4a2d9671b3dcbeb4b7de4c71c03555c0c2ae4862
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-09-17 08:27:00 +0200

    practised combobox and listbox manipulation in visual basic windows form

---
+++ Combobox_manipulation/Car_selector/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Populating combo box one
+        ComboBox1.Items.Add("Honda")
+        ComboBox1.Items.Add("Ford")
+        ComboBox1.Items.Add("BMW")
+        ComboBox1.Items.Add("Toyota")
+        ComboBox1.Items.Add("Mazda")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Populating combobox two
+        If ComboBox1.SelectedItem = "Honda" Then
+            ComboBox2.Items.Add("Fit")
+            Label3.Text = "The selected model is: FIT "
+        ElseIf ComboBox1.SelectedItem = "Ford" Then
+            ComboBox2.Items.Add("Focus")
+            Label3.Text = "The selected model is: FOCUS "
+        ElseIf ComboBox1.SelectedItem = "BMW" Then
+            ComboBox2.Items.Add("X5")
+            Label3.Text = "The selected model is: X5 "
+        ElseIf ComboBox1.SelectedItem = "Toyota" Then
+            ComboBox2.Items.Add("Corolla")
+            Label3.Text = "The selected model is: COROLLA "
+        ElseIf ComboBox1.SelectedItem = "Mazda" Then
+            ComboBox2.Items.Add("CX-5")
+            Label3.Text = "The selected model is: CX-5 "
+        End If
+    End Sub
+End Class
---
+++ Combobox_manipulation/Movie_selector/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.Items.Add("Action")
+        ComboBox1.Items.Add("Comedy")
+        ComboBox1.Items.Add("Science-Fiction")
+        ComboBox1.Items.Add("Horror")
+        ComboBox1.Items.Add("Romance")
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.SelectedItem = "Action" Then
+            ListBox1.Items.Add("Die-Hard")
+            ListBox1.Items.Add("Mad Max")
+            ListBox1.Items.Add("The dark night")
+            ListBox1.Items.Add("John Weak")
+            ListBox1.Items.Add("Gladiator")
+        ElseIf ComboBox1.SelectedItem = "Commedy" Then
+            ListBox1.Items.Add("The hangover")
+            ListBox1.Items.Add("Super bad")
+            ListBox1.Items.Add("Step brothers")
+            ListBox1.Items.Add("Grey hound")
+            ListBox1.Items.Add("Anchorman")
+        ElseIf ComboBox1.SelectedItem = "Science-Fiction" Then
+            ListBox1.Items.Add("Inception")
+            ListBox1.Items.Add("Interstellar")
+            ListBox1.Items.Add("The matrix")
+        ElseIf ComboBox1.SelectedItem = "Horror" Then
+            ListBox1.Items.Add("The exocist")
+            ListBox1.Items.Add("The conjuring")
+        End If
+
+    End Sub
+End Class
