d57d6baf092491f3d3f934d7a08a5262040eb27d
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-09-18 02:09:34 +0200

    practised date and time in visual basic console and windows form

---
+++ date_and_time/overdue_payment_calculater/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim rental_start_date As DateTime = DateTimePicker1.Value
+        Dim expected_return_date As DateTime = DateTimePicker2.Value
+        Dim actual_return_date As DateTime = DateTimePicker3.Value
+        Dim rental_fee As Double = 5
+        Dim total_amount_payable_after_penalties As Double
+        Dim total_amount_payed_before_penalties As Double
+
+
+
+        Dim total_number_of_days As Long
+
+        total_number_of_days = DateDiff(DateInterval.Day, rental_start_date, expected_return_date)
+        ListBox1.Items.Add($"You have rented our product for: {total_number_of_days}")
+
+
+
+        If actual_return_date > expected_return_date Then
+            total_amount_payable_after_penalties = (15 / 100) + (total_number_of_days * rental_fee)
+            ListBox1.Items.Add($"The amount payed after penalties is: { total_amount_payable_after_penalties}")
+        Else
+            total_amount_payed_before_penalties = total_number_of_days * rental_fee
+            ListBox1.Items.Add($"The total amount payed is: {total_amount_payed_before_penalties}")
+        End If
+
+    End Sub
+End Class
---
+++ date_and_time/total_payment_with_tax/Module1.vb
@@ -0,0 +1,38 @@
+﻿Module Module1
+    'Build a console program where the user inputs the rental start date, return date, and
+    'daily rental fee. The program calculates the total rental days And the total amount
+    'payable, including a 12% tax. Output the total amount
+
+    Sub Main()
+        Dim rental_start_date As DateTime
+        Dim rental_return_date As DateTime
+        Dim total_rental_days As Long
+        Dim daily_rental_fee As Double = 2
+        Dim total_amount_payable_before_taxes As Double
+        Dim total_amount_payable_after_taxes As Double
+
+
+        Console.WriteLine("Please enter Rental start date:")
+        rental_start_date = Console.ReadLine()
+
+        Console.WriteLine("Please enter Rental return date:")
+        rental_return_date = Console.ReadLine()
+
+
+        total_rental_days = DateDiff(DateInterval.Day, rental_start_date, rental_return_date)
+        'total_rental_days = (rental_return_date - rental_start_date).Days
+        Console.WriteLine("Total rental days amounts to: {0}", total_rental_days)
+
+        total_amount_payable_before_taxes = daily_rental_fee * total_rental_days
+
+        Console.WriteLine("Total amount payable before taxes is: {0}", total_amount_payable_before_taxes)
+
+        total_amount_payable_after_taxes = (12 / 100) * total_amount_payable_before_taxes
+        Console.WriteLine("Total amount payable after taxes is: {0}", total_amount_payable_after_taxes)
+
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
