fc4209f7b5e8a3db8e0a3d8fc695c802842dcb0f
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-09-19 23:51:49 +0200

    practised visual basic simple arithmetic solutions

---
+++ Simple Arithmetic/Jane_typist/Module1.vb
@@ -0,0 +1,42 @@
+﻿Module Module1
+    'Jane is a typist who works freelance and she charges the following rates:
+    'A page typed Single spacing $4.75
+    'A page typed Double spacing $3.50
+    'The user must enter the number Of pages she typed In Single spacing And the number Of pages she
+    'typed in double spacing. Calculate the amount due. She saves 15% of this amount to buy supplies.
+    'Write a console application To display the net amount she earned For her work.
+
+    Sub Main()
+        Dim single_spacing As Decimal = 4.75
+        Dim double_spacing As Decimal = 3.5
+
+        Dim single_spacing_page_number As Integer
+        Dim double_spacing_page_number As Integer
+        Dim total_amount_before_deduction As Decimal
+        Dim total_amount_earned_after_deduction As Decimal
+
+
+        Console.WriteLine("Please enter the number of pages for single spacing")
+        single_spacing_page_number = Console.ReadLine()
+
+        Console.WriteLine("Please enter the number of pages for double spacing")
+        double_spacing_page_number = Console.ReadLine()
+
+        total_amount_before_deduction = (single_spacing_page_number * single_spacing) + (double_spacing_page_number - double_spacing)
+        Console.WriteLine($"The total amount earned before deductions is: {total_amount_before_deduction}")
+
+        total_amount_earned_after_deduction = (single_spacing_page_number * single_spacing) + (double_spacing_page_number - double_spacing) - 0.15
+        Console.WriteLine("The total weekly amount earned after deductions is {0}", total_amount_earned_after_deduction)
+
+
+
+        Console.ReadKey()
+
+
+
+
+
+
+    End Sub
+
+End Module
---
+++ Simple Arithmetic/Nenyasha_weekly_wage/Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+    'Nenyasha works as a part time assistant in a book shop and receives an hourly wage. Enter the number
+    'of hours he worked for a specific week, as well as the pay rate per hour. He has to pay $8 per week
+    'towards the recreation club. Write a console application To calculate And display how much he has
+    'earned for the week.
+
+    Sub Main()
+        Dim hourly_wage As Double
+        Dim hours_worked As Double
+        Dim total_salary As Double
+        Dim recreational_club_fee As Double = 8
+
+
+
+        Console.WriteLine("Please enter Nenyasha's hourly wage: ")
+        hourly_wage = Console.ReadLine()
+
+        Console.WriteLine("Enter the number of hours he worked: ")
+        hours_worked = Console.ReadLine()
+
+        total_salary = (hourly_wage * hours_worked) - recreational_club_fee
+
+        Console.WriteLine("The amount he earned this week is {0}", total_salary)
+
+
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
