a28ad49abc4e06821350d1cbd64468333fe604c9
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-09-22 23:49:48 +0200

    practised visualbasic structures

---
+++ Structure/car_year_comparison/Module1.vb
@@ -0,0 +1,11 @@
+﻿Module Module1
+    'Write a Structure named Car With Make (String), Model (String), And Year
+    '(Integer). Implement a method called CompareByYear that takes another Car object And
+    'returns an Integer indicating whether the current car Is older, newer, Or the same year As
+    'the other car.
+
+    Sub Main()
+
+    End Sub
+
+End Module
