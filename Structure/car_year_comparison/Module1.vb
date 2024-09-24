112fc9c712a043c23ed5cdca1980de591ac5d169
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-09-24 23:56:43 +0200

    refactored car year comparison question using structures

--- Structure/car_year_comparison/Module1.vb
+++ Structure/car_year_comparison/Module1.vb
@@ -4,7 +4,14 @@
     'returns an Integer indicating whether the current car Is older, newer, Or the same year As
     'the other car.
 
+    Public Structure Car
+        Dim Make As String
+        Dim Model As String
+        Dim year As Integer
+
+    End Structure
     Sub Main()
+        Console.Write("hello")
 
     End Sub
 
