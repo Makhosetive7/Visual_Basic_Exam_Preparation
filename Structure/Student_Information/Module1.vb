0a463e09cce12952c80245f4eacf9c92c958006a
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-09-26 22:04:41 +0200

    rounding up on visual basic structures

---
+++ Structure/Student_Information/Module1.vb
@@ -0,0 +1,45 @@
+﻿Module Module1
+    'Define a structure named Student that stores the following details:
+    '• Student ID
+    '• Name
+    '• Course
+    '• Grade
+    'Allow the user To input the details For 5 students, store them In an array Of Student structures, And print out the list
+    'of students And their grades.
+
+    Public Structure Student
+        Dim student_id As Double
+        Dim name As String
+        Dim course As String
+        Dim grade As Char
+    End Structure
+    Sub Main()
+
+        Dim student_data(2) As Student
+
+        For i As Integer = 0 To 2
+            Console.WriteLine("Please enter student Identity:")
+            student_data(i).student_id = Console.ReadLine()
+
+            Console.WriteLine("Please enter student name:")
+            student_data(i).name = Console.ReadLine()
+
+            Console.WriteLine("Please enter student course:")
+            student_data(i).course = Console.ReadLine()
+
+            Console.WriteLine("Please enter student grade:")
+            student_data(i).grade = Console.ReadLine()
+        Next
+
+        Console.WriteLine("{0, -10} {1, -10} {2, -10} {3, -10}", "student_id", "name", "couse", "grade")
+        Console.WriteLine(New String("-"c, 55))
+
+        For i As Integer = 0 To 2
+            Console.WriteLine("{0, -10} {1, -10} {2, -10} {3, -10}", student_data(i).student_id, student_data(i).name, student_data(i).course, student_data(i).grade)
+        Next
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ Structure/employee_details/Module1.vb
@@ -0,0 +1,48 @@
+﻿Module Module1
+    'Create a Visual Basic structure named Employee that stores the following details for each employee:
+    '• Name
+    '• Age
+    '• Job Title
+    '• Salary
+    'Write a program that accepts input For 3 employees, stores their details, And displays the information For Each
+    'employee.
+
+    Public Structure Employee
+        Dim name As String
+        Dim age As Integer
+        Dim job_title As String
+        Dim salary As Decimal
+    End Structure
+    Sub Main()
+
+        Dim employee_data(2) As Employee
+
+        For i As Integer = 0 To 2
+            Console.WriteLine("Please enter employee name:")
+            employee_data(i).name = Console.ReadLine()
+
+            Console.WriteLine("Please enter employee age:")
+            employee_data(i).age = Console.ReadLine()
+
+            Console.WriteLine("Please enter employee Job Title:")
+            employee_data(i).job_title = Console.ReadLine()
+
+            Console.WriteLine("Please enter employee salary:")
+            employee_data(i).salary = Console.ReadLine()
+        Next
+
+        For i As Integer = 0 To 2
+            Console.WriteLine("Employee {0} details", i + 1)
+            Console.WriteLine("Name: " & employee_data(i).name)
+            Console.WriteLine("Age: " & employee_data(i).age)
+            Console.WriteLine("Job Title: " & employee_data(i).job_title)
+            Console.WriteLine("Salary: " & employee_data(i).salary)
+        Next
+
+
+        Console.ReadKey()
+
+    End Sub
+
+
+End Module
