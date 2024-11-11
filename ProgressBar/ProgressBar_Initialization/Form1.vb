6c4c6a436740a8668570890f0807740dd484219b
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-11-11 10:15:38 +0200

    good morning
    working on windows form visual basic progress bar manipulation

---
+++ ProgressBar/ProgressBar_Initialization/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    'Write a Visual Basic code snippet to initialize a ProgressBar control with a minimum value of 0, a
+    'maximum value Of 100, And a starting value Of 0
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ProgressBar1.Value = 0
+
+        ProgressBar1.Minimum = 0
+        ProgressBar1.Maximum = 100
+
+        For i As Integer = ProgressBar1.Minimum To ProgressBar1.Maximum Step 10
+            ProgressBar1.Value = i
+            System.Threading.Thread.Sleep(100)
+
+            If ProgressBar1.Value = 100 Then
+                MessageBox.Show("progress bar has reached 100%")
+            End If
+        Next
+
+
+    End Sub
+
+End Class
