f5cdfddb0d56e5aa07c048fc24dee37d54d49f46
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-11-17 13:32:49 +0200

    practised progressbar and date time questions

---
+++ ProgressBar/Progress_bar_increamental/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    'Write a program in Visual Basic that uses a For...Next loop to increase the value of a ProgressBar
+    'by 5 units on each iteration until it reaches the maximum value. Display a message box saying
+    '"Completed" when it reaches 100.
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ProgressBar1.Value = 0
+        ProgressBar1.Minimum = 0
+        ProgressBar1.Maximum = 100
+
+
+        For i As Integer = ProgressBar1.Minimum To ProgressBar1.Maximum
+            ProgressBar1.Value = i
+
+            If ProgressBar1.Value = 100 Then
+                MessageBox.Show("Progress bar has reached 100%", "success", MessageBoxButtons.OK, MessageBoxIcon.Hand)
+            End If
+        Next
+    End Sub
+
+
+
+
+End Class
---
+++ date_and_time/rental_period_day_hours_minutes/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+    'Develop a console program that calculates and displays the rental period in days, hours,
+    'And minutes. The user provides the rental start And return dates And times. Output the
+    'period in days, hours, And minutes format.
+    '---o Hint: Use TimeSpan To break down the difference into days, hours, And minutes.
+
+
+    Sub Main()
+        Dim rental_start_date As DateTime
+        Dim rental_return_date As DateTime
+        Dim result_date As TimeSpan
+
+        Console.WriteLine("Please enter rental start date:")
+        rental_start_date = Console.ReadLine()
+
+        Console.WriteLine("Please enter rental return date:")
+        rental_return_date = Console.ReadLine()
+
+
+        result_date = (rental_return_date - rental_start_date)
+
+        Console.WriteLine("the rental return date is in {0}days {1}hours and {2}minutes", result_date.Days, result_date.Hours, result_date.Minutes)
+
+        Console.ReadKey()
+    End Sub
+
+End Module
