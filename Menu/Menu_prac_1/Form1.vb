60152742be322f783aeef7b773810cb1ca670161
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-11-18 11:26:37 +0200

    practised treeview and menus in visual basic

---
+++ Menu/Menu_prac_1/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim top_menu_bar As New MainMenu()
+
+
+        Dim main_menu_item_File As New MenuItem("&File")
+        Dim main_menu_item_Edit As New MenuItem("&Edit")
+
+        top_menu_bar.MenuItems.Add(main_menu_item_File)
+        top_menu_bar.MenuItems.Add(main_menu_item_Edit)
+
+        Dim sub_menu_item_New As New MenuItem("&New")
+        Dim sub_menu_item_Open As New MenuItem("&Open")
+        Dim sub_menu_item_Save As New MenuItem("&Save")
+
+        main_menu_item_File.MenuItems.Add(sub_menu_item_New)
+        main_menu_item_File.MenuItems.Add(sub_menu_item_Open)
+        main_menu_item_File.MenuItems.Add(sub_menu_item_Save)
+
+
+        Dim sub_menu_item_copy As New MenuItem("&Copy")
+        Dim sub_menu_item_cut As New MenuItem("&Cut")
+        Dim sub_menu_item_paste As New MenuItem("&Paste")
+
+
+        main_menu_item_Edit.MenuItems.Add(sub_menu_item_copy)
+        main_menu_item_Edit.MenuItems.Add(sub_menu_item_cut)
+        main_menu_item_Edit.MenuItems.Add(sub_menu_item_paste)
+
+        Me.Menu = top_menu_bar
+    End Sub
+End Class
---
+++ TreeView/cellphone_bramds/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "cellphone_brands_treeview"
+
+
+        Dim nokia_Brands As TreeNode = TreeView1.Nodes.Add("nokia_Brands_treeView")
+
+        nokia_Brands.Nodes.Add("Nokia Lumia")
+        nokia_Brands.Nodes.Add("Nokia C2")
+        nokia_Brands.Nodes.Add("Nokia Alphine")
+
+        Dim sumsung_Brands As TreeNode = nokia_Brands.Nodes.Add("sumsung_Brands_treeview")
+
+        sumsung_Brands.Nodes.Add("Sumsung S10")
+        sumsung_Brands.Nodes.Add("Sumsung A05")
+        sumsung_Brands.Nodes.Add("sumsung b16")
+
+        Dim huawai_Brands As TreeNode = sumsung_Brands.Nodes.Add("huawai_Brands_treeview")
+
+        huawai_Brands.Nodes.Add("Huawai Y3")
+        huawai_Brands.Nodes.Add("Huawai Flimo")
+        huawai_Brands.Nodes.Add("Huawai Proband")
+
+
+
+
+
+
+
+
+
+    End Sub
+End Class
