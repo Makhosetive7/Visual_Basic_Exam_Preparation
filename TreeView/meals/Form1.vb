ff6afe2d012e8a208a4192e4c06019149e6778c4
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-11-19 15:21:01 +0200

    good day
    practised implementation of visual basic menus and treeview

---
+++ TreeView/meals/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim African_food_recipe As TreeNode = TreeView1.Nodes.Add("African Meals")
+
+        African_food_recipe.Nodes.Add("Jollof rice")
+        African_food_recipe.Nodes.Add("Bunny chow")
+        African_food_recipe.Nodes.Add("Umxhanxa")
+
+        Dim mexican_food_recipe As TreeNode = African_food_recipe.Nodes.Add("Mexican Meals")
+
+        mexican_food_recipe.Nodes.Add("Tacos")
+        mexican_food_recipe.Nodes.Add("Enchiladas")
+        mexican_food_recipe.Nodes.Add("Guacamole")
+
+
+        Dim italian_food_recipe As TreeNode = mexican_food_recipe.Nodes.Add("Italian Meals")
+
+        italian_food_recipe.Nodes.Add("Spaghetti Carbonara")
+        italian_food_recipe.Nodes.Add("Tiramisu")
+    End Sub
+End Class
