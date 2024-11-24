1e16ec0aa4577be2a972655ab18061299ecfde07
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-11-24 10:33:01 +0200

    good morning, practised files and simple arithmetic questions

---
+++ Files/reading_writing_file_exam_test_scores/Module1.vb
@@ -0,0 +1,61 @@
+﻿Imports System.IO
+
+Module Module1
+
+    'Write a VB.NETconsole application that prompts the user to input a student's name and their test scores for three
+    'subjects. Calculate the average score and display the student's name, scores for each subject, and their average
+    'score. Ensure the program validates that the input scores are between 0 and 100. If any score is outside this range,
+    'display an error message and prompt the user to re-enter the score
+
+    Sub Main()
+        Dim file_path As String = "C:\Users\USER\Desktop\hello.txt"
+        Dim line As String
+
+        Using sw As StreamWriter = New StreamWriter(file_path)
+            Dim student_name As String
+            Dim test_score_1 As Integer
+            Dim test_score_2 As Integer
+            Dim test_score_3 As Integer
+            Dim total_test_score As Double
+            Dim average_score As Integer
+
+            Console.WriteLine("Please enter students name : ")
+            student_name = Console.ReadLine()
+
+            Console.WriteLine("Please enter Test score 1 : ")
+            test_score_1 = Integer.Parse(Console.ReadLine())
+
+            Console.WriteLine("Please enter Test score 2 : ")
+            test_score_2 = Integer.Parse(Console.ReadLine())
+
+            Console.WriteLine("Please enter Test score 3 : ")
+            test_score_3 = Integer.Parse(Console.ReadLine())
+
+            total_test_score = test_score_1 + test_score_2 + test_score_3
+
+            average_score = total_test_score / 3
+
+            sw.WriteLine("The student name is : {0}" & Environment.NewLine & "Test score 1 is : {1}" & Environment.NewLine & "Test score 2 is : {2}" & Environment.NewLine & "Test score 3 is : {3}" & Environment.NewLine & "The avarage Test score is : {4}", student_name, test_score_1, test_score_2, test_score_3, average_score)
+            Console.WriteLine("The student name is : {0}" & Environment.NewLine & "Test score 1 is : {1}" & Environment.NewLine & "Test score 2 is : {2}" & Environment.NewLine & "Test score 3 is : {3}" & Environment.NewLine & "The avarage Test score is : {4}", student_name, test_score_1, test_score_2, test_score_3, average_score)
+        End Using
+
+
+        'reading from file
+        If File.Exists(file_path) Then
+            Using sr As StreamReader = New StreamReader(file_path)
+                line = sr.ReadLine()
+
+                While (line <> Nothing)
+                    Console.WriteLine("----------------------------------------------------------------------------")
+                    Console.WriteLine(line)
+                    line = sr.ReadLine()
+                End While
+            End Using
+        End If
+
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ Random questions/factorial_cube_square_root/Module1.vb
@@ -0,0 +1,57 @@
+﻿Module Module1
+    Sub Main()
+
+        ' Finding square root
+        Dim number_to_square As Integer
+        Dim result As Double
+
+        Console.WriteLine("Please enter number to square: ")
+        number_to_square = Integer.Parse(Console.ReadLine())
+
+        result = Math.Sqrt(number_to_square)
+
+        Console.WriteLine("The square root of {0} is: {1}", number_to_square, result)
+
+
+        'Finding Power
+        Dim base_number As Double
+        Dim exponent_number As Double
+        Dim powered_result As Double
+
+        Console.WriteLine("Please enter base number :")
+        base_number = Integer.Parse(Console.ReadLine())
+
+        Console.WriteLine("Please enter exponent number :")
+        exponent_number = Integer.Parse(Console.ReadLine())
+
+        powered_result = Math.Pow(base_number, exponent_number)
+
+        Console.WriteLine("The power of given number is: {0}", powered_result)
+
+        'Finding cube
+        Dim cubed_number As Double
+        Dim cubed_result As Double
+
+        Console.WriteLine("Please enter number to be cubed :")
+        cubed_number = Integer.Parse(Console.ReadLine())
+
+        cubed_result = cubed_number ^ (1 / 3)
+
+        Console.WriteLine("The cubed result is: {0}", cubed_result)
+
+        'Finding factorial
+        Dim number As Integer
+        Dim factorial As Long = 1
+
+        Console.WriteLine("Please enter number: ")
+        number = Integer.Parse(Console.ReadLine())
+
+        For i As Integer = 1 To number
+            factorial *= i
+        Next
+
+        Console.WriteLine("The factorial of {0} is {1}", number, factorial)
+
+        Console.ReadKey()
+    End Sub
+End Module
